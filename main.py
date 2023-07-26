7a4a19de171451767f3c61d0ba5f88762d984595
Huesos Pidorasovich <maxkozlov132435@gmail.com>
2023-07-26 19:09:44 +0300

    added Hexagon class

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from pyglet import *
 
+class Hexagon:
+    def __init__(self, x, y, r, color=(255, 255, 255, 255), batch=None, group=None):
+        self.shape = shapes.Polygon([x + r, y], [x + r/2, y + r * 3 ** 0.5/2], [x - r/2, y + r * 3 ** 0.5/2], [x - r, y], [x - r/2, y - r * 3 ** 0.5/2], [x + r/2, y - r * 3 ** 0.5/2], color=color, batch=batch)
+    
+    def draw(self):
+        self.shape.draw()
+
 
 resolutions = ((1280, 720), (1920, 1080), (3840, 2160), (2560, 1440))
 
@@ -9,14 +16,15 @@ batch = graphics.Batch()
 
 bg = shapes.Rectangle(x=0, y=0, width=3000, height=3000, color=(250, 250, 230), batch=batch)
 # я не придумал как нарисовать hexagon
-hexagon = shapes.Rectangle(x=500, y=500, width=400, height=400, batch=batch)
+#hexagon = shapes.Polygon([100, 100], [200, 100], [250, 200], [200, 300], [100, 300], [50, 200], color=(100, 100, 100), batch=batch)
+hexagon = Hexagon(100, 100, 100, color=(100, 100, 100), batch=batch)
 
 
 @win.event
 def on_draw():
     win.clear()
     batch.draw()
-    hexagon.draw()
+    #hexagon.draw()
 
 
 @win.event
