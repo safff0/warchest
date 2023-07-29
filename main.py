45bca1126fd2871641b1acc036890453dc19fe1b
safff0 <116974687+safff0@users.noreply.github.com>
2023-07-29 23:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,32 +8,39 @@ class Hexagon:
         self.shape.draw()
 
 
-resolutions = ((1280, 720), (1920, 1080), (3840, 2160), (2560, 1440))
+class Window(window.Window):
+    resolutions = ((640, 360), (1280, 720), (1920, 1080), (3840, 2160), (2560, 1440))
 
-win = window.Window(1280, 720, resizable=True)
-win.config.alpha_size = 8
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_minimum_size(640, 360)
+        self.win_width = 1280
+        self.win_height = 720
+        self.objects = []
+        self.push_handlers(on_resize=self.loc_on_resize)
+
+    def loc_on_resize(self, width, height):
+        if (width, height) in Window.resolutions:
+            return
+        width, height = min(Window.resolutions, key=lambda x: abs(x[0] - width))
+        self.win_width, self.win_height = width, height
+            
+    def on_draw(self):
+        self.set_size(self.win_width, self.win_height)
+        self.clear()
+        for obj in self.objects:
+            obj.draw()
+
+
+win = Window(1280, 720, resizable=True)
 batch = graphics.Batch()
 
 bg = shapes.Rectangle(x=0, y=0, width=3000, height=3000, color=(250, 250, 230), batch=batch)
 # я не придумал как нарисовать hexagon
 #hexagon = shapes.Polygon([100, 100], [200, 100], [250, 200], [200, 300], [100, 300], [50, 200], color=(100, 100, 100), batch=batch)
 hexagon = Hexagon(100, 100, 100, color=(100, 100, 100), batch=batch)
-
-
-@win.event
-def on_draw():
-    win.clear()
-    batch.draw()
-    #hexagon.draw()
-
-
-@win.event
-def on_resize(width, height):
-    if (width, height) in resolutions:
-        return
-    res = min(resolutions, key=lambda x: abs(x[0] - width))
-    width, height = res
-    win.set_size(width, height)
+win.objects.append(bg)
+win.objects.append(hexagon)
 
 
 app.run()
